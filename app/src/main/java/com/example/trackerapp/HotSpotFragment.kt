39d6ae402acb65b7581069e1fe690c30ad7b3c92
Colin <beutt102@mail.chapman.edu>
2023-04-02 11:09:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/trackerapp/HotSpotFragment.kt
@@ -0,0 +1,18 @@
+package com.example.trackerapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HotSpotFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.hotspot_layout, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/trackerapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.trackerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.trackerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav
+
+        supportFragmentManager.commit {
+            add(R.id.frame_content, HotSpotFragment())
+        }
+
+    }
+}
