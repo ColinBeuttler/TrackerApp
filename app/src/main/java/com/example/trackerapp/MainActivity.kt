741bf0c6b9a69395411fa601fdbb9d827f598699
Colin <beutt102@mail.chapman.edu>
2023-04-06 11:09:43 -0700

    removed unneccesary add fragment and setcontent view

--- app/src/main/java/com/example/trackerapp/MainActivity.kt
+++ app/src/main/java/com/example/trackerapp/MainActivity.kt
@@ -11,54 +11,45 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNav.setOnItemSelectedListener (this)
-        supportFragmentManager.commit {
-            add(R.id.frame_content, HotspotFragment())
-        }
-
+        binding.bottomNav.setOnItemSelectedListener(this)
 
     }
 
-    private fun onPhotosClicked(){
+    private fun onPhotosClicked() {
         supportFragmentManager.commit {
             replace(R.id.frame_content, PhotosFragment())
         }
     }
 
-    private fun onSpottingClicked(){
+    private fun onSpottingClicked() {
         supportFragmentManager.commit {
             replace(R.id.frame_content, SpottingFragment())
         }
     }
 
-    private fun onHotSpotClicked(){
+    private fun onHotSpotClicked() {
         supportFragmentManager.commit {
             replace(R.id.frame_content, HotspotFragment())
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.nav_spotting){
+        if (item.itemId == R.id.nav_spotting) {
             onSpottingClicked()
             return true
-        }
-        else if (item.itemId == R.id.nav_hotspot){
+        } else if (item.itemId == R.id.nav_hotspot) {
             onHotSpotClicked()
             return true
-        }
-        else if (item.itemId == R.id.nav_photos){
+        } else if (item.itemId == R.id.nav_photos) {
             onPhotosClicked()
             return true
-        }
-        else{
+        } else {
             return false
         }
     }
