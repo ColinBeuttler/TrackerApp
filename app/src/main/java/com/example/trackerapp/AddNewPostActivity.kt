cae255fd280528f340970e70c23ecd142152b26b
Colin <beutt102@mail.chapman.edu>
2023-04-12 11:31:07 -0700

    update

--- app/src/main/java/com/example/trackerapp/AddNewPostActivity.kt
+++ app/src/main/java/com/example/trackerapp/AddNewPostActivity.kt
@@ -2,10 +2,14 @@ package com.example.trackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trackerapp.databinding.ActivityAddNewPostBinding
 
 class AddNewPostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNewPostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_new_post)
+        binding = ActivityAddNewPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
