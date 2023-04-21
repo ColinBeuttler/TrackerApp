0a3555cf59b247fc501709f2ea0a5806a7453bad
colin <beutt102@mail.chapman.edu>
2023-04-21 11:41:34 -0700

    added Post onClick

--- app/src/main/java/com/example/trackerapp/AddNewPostFragment.kt
+++ app/src/main/java/com/example/trackerapp/AddNewPostFragment.kt
@@ -1,13 +1,14 @@
 package com.example.trackerapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
-import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import com.example.trackerapp.databinding.AddNewPostLayoutBinding
+
 
 
 class AddNewPostFragment : Fragment() {
@@ -25,9 +26,17 @@ class AddNewPostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-
+        binding.postButton.setOnClickListener(View.OnClickListener { onClick() })
     }
 
+    private fun onClick() {
+        if(binding.textInputLocation.text?.isEmpty() == true){
+            Log.v(TAG, "Please Enter a location")
+        }
+        else{
+            Log.v(TAG, "Post Button activated")
+        }
+
+    }
 }
 
